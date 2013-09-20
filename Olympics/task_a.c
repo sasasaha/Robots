4eb2cf243e0a93250ec44200a7004d08a0f1260e
sasasaha <sasasasahaha@mail.ru>
2013-09-21 00:19:57 +0400

    Дописал пару (возможно) нужных функций + изменил систему позиционирования полей. (Антон, можно как-то уменьшить количество первых if'ов, когда меняю положение полей?)

--- Olympics/task_a.c
+++ Olympics/task_a.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 #include <conio.h>
+int min (int a, int b)
+{
+  if (a > b)
+	return b;
+  else
+	return a;
+}
+int max (int a, int b)
+{
+  if (a > b)
+	return a;
+  else
+	return b;
+}
+void swap (int *a, int *b)
+{
+  int n;
+
+  n = *a;
+  *a = *b;
+  *b = n;
+}
 void main (void)
 {
   FILE *F;
@@ -9,23 +31,18 @@ void main (void)
 	printf ("Error with reading file");
 
   fscanf (F, "%i%i%i%i%i%i", &FldOneH, &FldOneW, &FldTwoH, &FldTwoW, &FldThreeH, &FldThreeW);
+  printf ("1st: FldOneH: %i, FldOneW: %i, FldTwoH: %i, FldTwoW: %i, FldThreeH: %i, FldThreeW: %i \n", FldOneH, FldOneW, FldTwoH, FldTwoW, FldThreeH, FldThreeW);
 
-  printf ("FldOneH: %i, FldOneW: %i, FldTwoH: %i, FldTwoW: %i, FldThreeH: %i, FldThreeW: %i \n", FldOneH, FldOneW, FldTwoH, FldTwoW, FldThreeH, FldThreeW);
+  if (FldOneH < FldOneW)
+    swap (&FldOneH, &FldOneW);
+  if (FldTwoH < FldTwoW)
+    swap (&FldTwoH, &FldTwoW);
+  if (FldThreeH < FldThreeW)
+    swap (&FldThreeH, &FldThreeW);
 
-  if (FldOneH >= FldTwoH && FldOneH >= FldTwoW)
-  {
-	n = FldTwoH;
-    FldTwoH = FldTwoW;
-	FldTwoW = n;
-  }
-  else if (FldOneH <= FldTwoH && FldOneH <= FldTwoW)
-  {
-	n = FldOneH;
-    FldOneH = FldOneW;
-	FldOneW = n;
-  }
+  printf ("2nd: FldOneH: %i, FldOneW: %i, FldTwoH: %i, FldTwoW: %i, FldThreeH: %i, FldThreeW: %i \n\n", FldOneH, FldOneW, FldTwoH, FldTwoW, FldThreeH, FldThreeW);
 
-  if (FldOneW >= FldTwoW)
+  if (FldOneW >= FldTwoW) // first field + second field
     if (FldOneH >= FldTwoH)
       S = FldOneH * FldOneW;
 	else 
@@ -34,10 +51,23 @@ void main (void)
 	if (FldOneH < FldTwoH)
       S = FldTwoH * FldTwoW;
     else 
-	  S = FldTwoH * FldTwoW + (FldOneH * FldOneW - FldTwoH * FldOneW);
+	  S = FldTwoH * FldTwoW + (FldOneH * FldOneW - FldTwoH * FldOneW); 
+
+  if (FldThreeH <= min (FldTwoH, FldOneH) && FldThreeH <= min (FldTwoW, FldOneW))
+  {
+	n = FldThreeH;
+    FldThreeH = FldOneW;
+	FldThreeW = n;
+  }
+  else if (FldThreeH <= max (FldTwoH, FldOneH) && FldThreeH <= max (FldTwoW, FldOneW))
+  {
+	n = FldThreeH;
+    FldThreeH = FldOneW;
+	FldThreeW = n;
+  }
+
   
- 
-  printf ("%i \n", S);
+  printf ("Field min area: %i \n", S);
 
   getch();
 }
